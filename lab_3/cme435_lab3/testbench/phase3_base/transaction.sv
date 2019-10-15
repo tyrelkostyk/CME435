e6ac530fc50dc66866b43ea1a063f155c082903b
Tyrel Kostyk <tck290@mail.usask.ca>
2019-10-15 16:08:38 -0600

    finished phase 3 (rough draft - it compiles).

--- lab_3/cme435_lab3/testbench/phase3_base/transaction.sv
+++ lab_3/cme435_lab3/testbench/phase3_base/transaction.sv
@@ -6,18 +6,34 @@ class transaction;
 
 // ************************* INSTANTIATIONS ************************* //
 
-rand bit [7:0] data_in;
+rand bit [7:0] dest_addr;		// random dest addr. Can be 1, 2, 3, or 4
+rand bit [7:0] data_in[];		// payload array; can be 4, 8, 12, or 16 bytes
 
-bit [7:0] data_out_1, data_out_2, data_out_3, data_out_4;
+bit [7:0] data_out_1[], data_out_2[], data_out_3[], data_out_4[];
 
+
+constraint payload_size_c {
+	data_in.size inside { 4, 8, 12, 16 };
+}
+
+constraint payload_dest_addr_c {
+	dest_addr inside { [1:4] };
+}
 
 // ******************* FUNCTIONS AND CONSTRUCTORS ******************* //
 
 function void display_trans( string name );
-	$display("- %s ",name);
-	$display("-------------------------");
-	$display("- a = %0d, b = %0d",a,b);
-	$display("- c = %0d",c);
+	$display(  "-- %s : ----- PACKET -----", name);
+	$display(  "--------------------------");
+	$display(  "DEST_ADDR    : - 0 : %h ", dest_addr);
+	foreach( data_in[i] ) begin
+		$display("DATA_IN      : %3d : %h ", i, data_in[i]);
+		$display("DATA_OUT_1   : %3d : %h ", i, data_out_1[i]);
+		$display("DATA_OUT_2   : %3d : %h ", i, data_out_2[i]);
+		$display("DATA_OUT_3   : %3d : %h ", i, data_out_3[i]);
+		$display("DATA_OUT_4   : %3d : %h ", i, data_out_4[i]);
+	end
+
 endfunction
 
 endclass
